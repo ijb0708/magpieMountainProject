e2fce36ec9fab306bb61c5ff4ed8c6b49fcc1a26
whitepaper <ijb0708>
2022-05-12 20:27:36 +0900

    ver 0.1.0
    1차 정리본 시퀄라이저 는 미작동

--- backendServer/src/app.js
+++ backendServer/src/app.js
@@ -1,19 +1,26 @@
 import express from 'express';
 // modules
-import path from 'path';
-import fs from 'fs';
 import bodyParser from 'body-parser';
+import helmet from 'helmet'
+import sequelize from './plugins/sequelizer/index';
+import logger from './plugins/logger/index.js';
 // 설정파일
 //import config from './config';
 import routes from './routes/index.js';
 
-const __dirname = path.resolve();
-const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'src','config.json'), 'utf-8')).dev;
-
 const app = express();
 
+sequelize.sync({force: false})
+.then(() => {
+    logger.info("DB conn Succes");
+}).catch(err => {
+    logger.info("DB conn Error");
+})
+
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/', routes);
+
 export default app;
