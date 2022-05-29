76604d500007ef3b0c674acaa05365471fbb4129
whitepaper <ijb0708>
2022-05-29 22:18:21 +0900

    ver 0.3.3 alpha
    coop 문제

--- backendServer/src/app.js
+++ backendServer/src/app.js
@@ -25,6 +25,12 @@ pool.connect(err => {
     }
 });
 
+app.use(function(req, res, next) {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+})
+
 // 
 app.use(cors({
     origin:[
